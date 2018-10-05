91f4a772feeb5d6031253251b9a5845de93eb871
kantor <maspamzdev@gmail.com>
2018-10-05 08:28:24 +0700

    maspamz
    -

--- soal4/Soal4/main.cpp
+++ soal4/Soal4/main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
-
 using namespace std;
 
-int main ()
-{
-  int x,y,ax,ay,bx,by,cx,cy,t=0;
-  cout << "Masukkan Total Kepala Robot : ";
-  cin >> x;
-  cout << "Masukkan Total Kaki Robot : ";
-  cin >> y;
+int main(void){
+    int A, B, C, Ha, Hb, Head, Foot;
+    int Flag=0;
+    A=0;
 
-  while(t<=50)
-  cout << "The value you entered is " << i;
-  cout << " and its double is " << i*2 << ".\n";
-  return 0;
+    cout << "Masukan Jumlah Kepala : ";
+    cin >> Head;
+
+    cout << "Masukan Jumlah Kaki : ";
+    cin >> Foot;
+
+    while(Flag==0 && A < Head/3){
+        A++;
+        Ha = Head/3;
+        Hb = Ha/2;
+        A=Hb;
+        B=Hb;
+        C=Hb;
+        if((A+B*2+C*3)==Head && (A*2+B*3+C*4) == Foot){
+                Flag=1;
+        }
+    }
+
+    if(Flag==1){
+        cout << "Total  robot = " << (A+B+C) << endl;
+        cout << " " << endl;
+        cout << "Robot A : " << A << "Robot B : " << B << "Robot C : " << C << endl;
+    }else{
+        cout << "DATA SALAH" << endl;
+    }
+
+    return 0;
 }
+
+
